80772bf5d4dfeee3194d5d8c35f3bf6e6634a6e9
Mathew-Varghese28 <mca526@rajagiri.edu>
2021-03-28 20:12:58 +0530

    Gym

---
+++ feedback.php
@@ -0,0 +1,116 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Registion</title>
+</head>
+<body background="https://images.pexels.com/photos/4753987/pexels-photo-4753987.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"  style="height: 100%; background-position: center; background-repeat: no-repeat; background-size: cover;">
+    <nav class="navbar navbar-light bg-light">
+        <div class="container-fluid">
+          <span class="navbar-text">
+            <div class="container">
+                <div class="row">
+                    <div class="col col-12 col-sm-2 col-lg-2 col-md-2 col-xl-2 col-xxl-2">
+                        <img height="10px" src="https://www.infomagic.com/uploads/2017/Jul/1500111801-999178017-0_thumb.blob" class="img-thumbnail" alt="...">
+                    </div>
+                    <div class="col col-12 col-sm-10 col-lg-10 col-md-10 col-xl-10 col-xxl-10">
+                        <h1 style="color: rgb(221, 122, 9); font: size 150px;;"><center>Adhens Gym </center> </h1>
+                        Holmes Place Athens is a premium health club that offers you fitness, nutrition, spa and other services for a balanced, healthy and enjoyable life
+                    </div>
+                </div>
+            </div>
+          </span>
+        </div>
+      </nav>
+    
+    
+    
+      <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="index.php">Adhens GYM</a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="index.php">Home</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="Photogalary.php">Photo Gallary</a>
+          </li>
+          <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+              QuickLinks
+            </a>
+            <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+              <li><a class="dropdown-item" href="Signin.php">Sign In</a></li>
+              <li><a class="dropdown-item" href="registration.php">Registration</a></li>
+              <li><hr class="dropdown-divider"></li>
+              <li><a class="dropdown-item" href="feedback.php">Feedback</a></li>
+            </ul>
+          </li>
+        </ul>
+        <form class="d-flex">
+          Phone No: +91 8547029350<br>
+          &emsp;&emsp;&emsp;&emsp;&emsp;: +91 8547029350
+          
+        </form>
+      </div>
+    </div>
+  </nav>
+        
+    
+    <div class="container">
+            <div class="row">
+                <div class="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+
+                </div>
+                <div class="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                    <h1 style="color: white;"><center>Feedback Form</center>
+                        
+<table class="table">
+    <input type="text" class="form-control" placeholder="Enter your Name"><br>
+    <input type="email" class="form-control" placeholder="Email"><br>
+    <textarea type="text" class="form-control" placeholder="Address"rows="4" cols="50"></textarea><br>
+    <input type="text" class="form-control" placeholder="Contact No"><br>
+    <input type="text" class="form-control" placeholder="Feedback">
+    <br>
+    <button class="btn btn-success">Submit</button>
+</table>
+                    </h1>
+                    
+                </div>
+            </div>
+    </div>
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js" integrity="sha384-JEW9xMcG8R+pH31jmWH6WWP0WintQrMb4s7ZOdauHnUtxwoG2vI5DkLtS3qm9Ekf" crossorigin="anonymous"></script>
+</body>
+</html>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ index.php
@@ -0,0 +1,147 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Adhens Gym</title>
+</head>
+<body>
+
+  <nav class="navbar navbar-light bg-light">
+    <div class="container-fluid">
+      <span class="navbar-text">
+        <div class="container">
+            <div class="row">
+                <div class="col col-12 col-sm-2 col-lg-2 col-md-2 col-xl-2 col-xxl-2">
+                    <img height="10px" src="https://www.infomagic.com/uploads/2017/Jul/1500111801-999178017-0_thumb.blob" class="img-thumbnail" alt="...">
+                </div>
+                <div class="col col-12 col-sm-10 col-lg-10 col-md-10 col-xl-10 col-xxl-10">
+                    <h1 style="color: rgb(221, 122, 9); font: size 150px;;"><center>Adhens Gym </center> </h1>
+                    Holmes Place Athens is a premium health club that offers you fitness, nutrition, spa and other services for a balanced, healthy and enjoyable life
+                </div>
+            </div>
+        </div>
+      </span>
+    </div>
+  </nav>
+
+
+
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="index.php">Adhens GYM</a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="index.php">Home</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="Photogalary.php">Photo Gallary</a>
+          </li>
+          <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+              QuickLinks
+            </a>
+            <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+              <li><a class="dropdown-item" href="Signin.php">Sign In</a></li>
+              <li><a class="dropdown-item" href="registration.php">Registration</a></li>
+              <li><hr class="dropdown-divider"></li>
+              <li><a class="dropdown-item" href="feedback.php">Feedback</a></li>
+            </ul>
+          </li>
+        </ul>
+        <form class="d-flex">
+          Phone No: +91 8547029350<br>
+          &emsp;&emsp;&emsp;&emsp;&emsp;: +91 8547029350
+          
+        </form>
+      </div>
+    </div>
+  </nav>
+    
+
+
+
+    <div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="carousel">
+        <div class="carousel-indicators">
+          <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+          <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+          <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+        </div>
+        <div class="carousel-inner">
+          <div class="carousel-item active">
+            <img src="https://img.webmd.com/dtmcms/live/webmd/consumer_assets/site_images/article_thumbnails/blog_posts/webmd-doctors/1800x1200_empty-gym.jpg" class="d-block w-100" alt="...">
+          </div>
+          <div class="carousel-item">
+            <img src="https://www.tuv.com/content-media-files/master-content/services/products/0168-tuv-rheinland-gym-equipment/tuv-rheinland-gym-equipment_core_2_2_1.jpg" class="d-block w-100" alt="...">
+          </div>
+          <div class="carousel-item">
+            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ7RE88h3hLjfjNNSHVHSYPbkb0y1E99ccIBQ&usqp=CAU" class="d-block w-100" alt="...">
+          </div>
+        </div>
+        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators"  data-bs-slide="prev">
+          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+          <span class="visually-hidden">Previous</span>
+        </button>
+        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators"  data-bs-slide="next">
+          <span class="carousel-control-next-icon" aria-hidden="true"></span>
+          <span class="visually-hidden">Next</span>
+        </button>
+      </div>
+
+
+      <div class="container">
+          <div class="row">
+            <div class="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+              <br>
+                <h2>
+                  Mission
+                </h2>
+                Our mission is to provide an Entertaining, Educational, Friendly and Inviting, Functional and Innovative experience of uncompromising quality that meets the health and fitness needs of the entire family.
+                <br><br> <br>
+                <h2>Objectives</h2>
+*To make best use of our equipment and resources through well-attended sessions <br>
+*To continue to improve our offer through increased number and diversity of sessions <br>
+*To use accessible venues and work towards a secure tenure <br>
+*To employ professional session leaders with a fair wage and fair contracts <br>
+            </div>
+            <div class="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+             <h2>Aims</h2> <br>
+*To build a culture of non-judgement and celebration of each other’s progress<br>
+*To build a supportive and inclusive culture, free from assumptions about identity and appearance<br>
+*To empower people to create healthy training habits<br>
+*To engage people who are excluded from most training facilities<br>
+*To provide high quality coaching and training facilities<br>
+*To have fair pricing models, remaining as affordable as possible<br>
+*To be a financially sustainable organisation, free from reliance on external funding<br>
+*To work with local community groups to improve our offer and accessibility for all members of our local communities. Further, we wish to reach out to communities most likely to feel excluded from mainstream commercial gyms.<br>
+*To empower people to improve their own physical and mental well-being<br>
+            </div>
+
+            
+            <div class="row">
+              <div class="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                <br>
+                  <h2>
+                    Description
+                  </h2>
+                  Adhens Gym is a health club company with facilities for sports, professional fitness, family recreation and spa. Under the Life Time Fitness® and Life Time Athletic® brands the company operates 112 centers in the United States and Canada, most of which operate 24 hours a day, seven days a week.
+              </div>
+          
+          </div>
+      </div>
+
+
+
+
+
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js" integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous"></script>
+</body>
+</html>
